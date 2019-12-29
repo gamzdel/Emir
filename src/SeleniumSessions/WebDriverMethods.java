27a1ddecfe136808963fbfa65330048a4c49a275
lankeshleader <49361514+lankeshleader@users.noreply.github.com>
2019-12-29 18:43:19 +0530

    webDriver Methods

--- src/SeleniumSessions/WebDriverMethods.java
+++ src/SeleniumSessions/WebDriverMethods.java
@@ -10,9 +10,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class WebDriverMethods {
 
